2a8a4390a8a9a746a06f2301307a08f9fbd73f7d
smeek123 <sean.meek06@gmail.com>
2023-05-05 16:42:02 -0400

    Initial Commit

---
+++ JamaGramswift/JamaGramswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamaGramswiftApp.swift
+//  JamaGramswift
+//
+//  Created by Sean P. Meek on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct JamaGramswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
