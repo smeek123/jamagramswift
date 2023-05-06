e64306436c4d70d771ce5229d0a1c054e340a301
smeek123 <sean.meek06@gmail.com>
2023-05-06 13:29:51 -0400

    tabview

---
+++ JamaGramswift/Views/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  JamaGramswift
+//
+//  Created by Sean P. Meek on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            Text("Favorites")
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+            
+            Text("Me")
+                .tabItem {
+                    Label("Me", systemImage: "person.circle")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JamaGramswift/Views/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  JamaGramswift
+//
+//  Created by Sean P. Meek on 5/6/23.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
