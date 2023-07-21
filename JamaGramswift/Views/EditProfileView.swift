fae7bf82b7aa10222d5d45f729d0e4feec4e6110
smeek123 <sean.meek06@gmail.com>
2023-07-21 10:32:34 -0400

    added photo picker to edit screen

--- JamaGramswift/Views/EditProfileView.swift
+++ JamaGramswift/Views/EditProfileView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
     @State private var username: String = ""
     @State private var name: String = ""
     @State private var bio: String = ""
     @State private var birthday: Date = Date()
+    @State private var selectedImage: Image? = nil
+    @State private var imageSelection: PhotosPickerItem? = nil
+    @State private var startingDate: Date = Date().addingTimeInterval(-3564259200)
+    @State private var endDate: Date = Date().addingTimeInterval(-410228000)
     
     var body: some View {
         VStack {
             VStack(spacing: 15) {
+                PhotosPicker(selection: $imageSelection, matching: .images) {
+                    if selectedImage != nil {
+                        selectedImage!
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                    } else {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                    }
+                }
+                .onChange(of: imageSelection) { _ in
+                    Task {
+                        if let data = try? await imageSelection?.loadTransferable(type: Data.self) {
+                            if let uiImage = UIImage(data: data) {
+                                selectedImage = Image(uiImage: uiImage)
+                                return
+                            }
+                        }
+                    }
+                }
+                
                 Section(header: HStack {
                     Text("Username")
                     
@@ -51,10 +79,44 @@ struct EditProfileView: View {
                         .background(Color(uiColor: .secondarySystemBackground))
                         .clipShape(Capsule())
                 }
+                
+                Section(header: HStack {
+                    Text("Birthday")
+                    
+                    Spacer()
+                }) {
+                    DatePicker("Birthday", selection: $birthday, in: startingDate...endDate, displayedComponents: .date)
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width * 0.95, height: 40)
+                        .background(Color(uiColor: .secondarySystemBackground))
+                        .clipShape(Capsule())
+                }
             }
             .padding()
             
             Spacer()
+            
+            Button {
+                
+            } label: {
+                HStack {
+                    Spacer()
+                    
+                    Text("Save")
+                        .foregroundColor(.primary)
+                        .font(.title3)
+                        .padding(.horizontal, 15)
+                    
+                    Spacer()
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.8, height: 25)
+                .padding(.horizontal, 15)
+            }
+            .background(Color("MainColor"))
+            .clipShape(Capsule())
+            .buttonStyle(.bordered)
+            .padding(10)
+            .padding(.vertical)
         }
         .navigationTitle("Customize")
     }
