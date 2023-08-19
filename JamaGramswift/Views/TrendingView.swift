7b808bec50fe59fb5c9502ce1f5b7e0525cf6907
smeek123 <sean.meek06@gmail.com>
2023-08-19 15:36:44 -0400

    added basic trending page ui

--- JamaGramswift/Views/TrendingView.swift
+++ JamaGramswift/Views/TrendingView.swift
@@ -10,34 +10,123 @@ import SwiftUI
 struct TrendingView: View {
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        Text("JamaGram")
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text("Top Artists")
                             .foregroundColor(.primary)
-                            .font(.system(size: 25))
+                            .font(.system(size: 30))
+                        
+                        Spacer()
                     }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 18)
                     
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink {
-                            Text("Notifications")
-                        } label: {
-                            Image(systemName: "bell")
-                                .foregroundColor(.primary)
-                                .font(.system(size: 20))
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 30) {
+                            ForEach(0..<10, id: \.self) { artist in
+                                VStack(spacing: 10) {
+                                    Circle()
+                                        .frame(width: 100, height: 100)
+                                    
+                                    Text(String(artist))
+                                        .foregroundColor(.primary)
+                                        .font(.system(size: 15))
+                                        .lineLimit(1)
+                                }
+                            }
                         }
-                    }
-                    
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink {
-                            Text("Messages")
-                        } label: {
-                            Image(systemName: "message")
-                                .foregroundColor(.primary)
-                                .font(.system(size: 20))
-                        }
+                        .padding(.horizontal, 18)
                     }
                 }
+                
+                VStack {
+                    HStack {
+                        Text("Top Tracks")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 30))
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 18)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 30) {
+                            ForEach(0..<10, id: \.self) { artist in
+                                VStack(spacing: 10) {
+                                    Rectangle()
+                                        .frame(width: 100, height: 100)
+                                    
+                                    Text(String(artist))
+                                        .foregroundColor(.primary)
+                                        .font(.system(size: 15))
+                                        .lineLimit(1)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 18)
+                    }
+                }
+                .padding(.vertical, 10)
+                
+                VStack {
+                    HStack {
+                        Text("Top Posts")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 30))
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 18)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 30) {
+                            ForEach(0..<10, id: \.self) { artist in
+                                VStack(spacing: 10) {
+                                    Rectangle()
+                                        .frame(width: 100, height: 100)
+                                    
+                                    Text(String(artist))
+                                        .foregroundColor(.primary)
+                                        .font(.system(size: 15))
+                                        .lineLimit(1)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 18)
+                    }
+                }
+                .padding(.vertical, 10)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("JamaGram")
+                        .foregroundColor(.primary)
+                        .font(.system(size: 25))
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        Text("Notifications")
+                    } label: {
+                        Image(systemName: "bell")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 20))
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink {
+                        Text("Messages")
+                    } label: {
+                        Image(systemName: "message")
+                            .foregroundColor(.primary)
+                            .font(.system(size: 20))
+                    }
+                }
+            }
         }
     }
 }
