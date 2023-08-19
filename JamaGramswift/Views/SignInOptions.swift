c6d559363cf69e72a8f1cc77c93c8bf0f5d701e5
smeek123 <sean.meek06@gmail.com>
2023-08-19 18:18:35 -0400

    added sign in with email view

---
+++ JamaGramswift/Views/SignInOptions.swift
@@ -0,0 +1,20 @@
+//
+//  SignInOptions.swift
+//  JamaGramswift
+//
+//  Created by Sean P. Meek on 8/19/23.
+//
+
+import SwiftUI
+
+struct SignInOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInOptions_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInOptions()
+    }
+}
--- JamaGramswift/Views/SignInView.swift
+++ JamaGramswift/Views/SignInView.swift
@@ -12,18 +12,36 @@ struct SignInView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack {
-            Text("JamaGram")
-                .foregroundColor(.primary)
-                .font(.system(size: 35))
-            
-            TextField("Enter your email address", text: $email)
-                .modifier(TextFieldModifier())
-            
-            SecureField("Enter your password", text: $password)
-                .modifier(TextFieldModifier())
-            
-            
+        NavigationStack {
+            VStack(spacing: 25) {
+                Spacer()
+                
+                Text("JamaGram")
+                    .foregroundColor(.primary)
+                    .font(.system(size: 35))
+                
+                TextField("Enter your email address", text: $email)
+                    .modifier(TextFieldModifier())
+                
+                SecureField("Enter your password", text: $password)
+                    .modifier(TextFieldModifier())
+                
+                Button {
+                    
+                } label: {
+                    LargeButtonView(title: "Log In")
+                }
+                
+                Spacer()
+                
+                NavigationLink {
+                    Text("")
+                } label: {
+                    Text("Don't have an account? Sign up now.")
+                        .foregroundColor(Color("MainColor"))
+                        .padding(.vertical)
+                }
+            }
         }
     }
 }
