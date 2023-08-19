c700d0c82e52928eda81231f2cfb846eaf5ea7f8
smeek123 <sean.meek06@gmail.com>
2023-08-19 09:15:07 -0400

    added search list

--- JamaGramswift/Views/SearchView.swift
+++ JamaGramswift/Views/SearchView.swift
@@ -13,18 +13,37 @@ struct SearchView: View {
     var body: some View {
         NavigationStack {
             ScrollView(showsIndicators: false) {
-                List {
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
-                    Text("10000")
+                LazyVStack(spacing: 24) {
+                    ForEach(0..<100) { user in
+                        HStack {
+                            Image("profile-image")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            Text("Wendy")
+                                .foregroundColor(.primary)
+                                .font(.system(size: 15))
+                            
+                            Spacer()
+                            
+                            Button {
+                                
+                            } label: {
+                                Text("Follow")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.primary)
+                            }
+                            .buttonBorderShape(.capsule)
+                            .buttonStyle(.borderedProminent)
+                            .tint(Color("MainColor"))
+                        }
+                        .padding(.horizontal, 18)
+                    }
                 }
                 .searchable(text: $search, prompt: "Search for friends or tracks")
+                .padding(.vertical, 10)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
