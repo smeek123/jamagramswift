d271e0582b2b9d7b7a9c75871f3ec7438bff8ee7
smeek123 <sean.meek06@gmail.com>
2023-08-20 15:05:49 -0400

    ireabase

--- JamaGramswift/JamaGramswiftApp.swift
+++ JamaGramswift/JamaGramswiftApp.swift
@@ -7,10 +7,21 @@
 
 import SwiftUI
 import AVKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 //main app that initializeses the app
 @main
 struct JamaGramswiftApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     //sets up the audio capabilities for the preview links
     func setAudioSession()  {
         let audioSession = AVAudioSession.sharedInstance()
